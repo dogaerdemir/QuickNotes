5f222ff6f12e4f64657cc12752832d0a41a8a58e
Doğa Erdemir <erdemir53@gmail.com>
2023-09-17 18:24:28 +0300

    Multi language support (English-Turkish) is now available

--- QuickNotes/View/Settings/SettingsViewController.swift
+++ QuickNotes/View/Settings/SettingsViewController.swift
@@ -6,18 +6,23 @@
 //
 
 import UIKit
+import Localize_Swift
 
-class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SettingsViewController: UIViewController {
     
-    let tableView: UITableView = {
-        let table = UITableView()
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        return table
-    }()
-    
+    @IBOutlet weak var tableView: UITableView!
+    var actionSheet: UIAlertController!
     var lightDarkSwitch = UISwitch()
     var biometricSwitch = UISwitch()
+    var languageButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 0, y: 0, width: 85, height: 20)
+        button.setTitle("tab_settings_language_button".localized(), for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        return button
+    }()
     let userDefaults = UserDefaults.standard
+    let availableLanguages = Localize.availableLanguages()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +31,7 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.navigationItem.title = "Settings"
+        self.navigationItem.title = "tab_settings".localized()
         
         let isDarkMode = UserDefaults.standard.bool(forKey: "isDarkMode")
         self.view.overrideUserInterfaceStyle = isDarkMode ? .dark : .light
@@ -42,35 +47,22 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
             biometricSwitch.isOn = appLocking ? true : false
         }
         
+        if let language = UserDefaults.standard.value(forKey: "language") as? String {
+            languageButton.setTitle("\(language) ↓", for: .normal)
+        } else {
+            languageButton.setTitle("tab_settings_language_button".localized(), for: .normal)
+        }
+        
         view.addSubview(tableView)
         
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.delegate = self
         tableView.dataSource = self
         tableView.frame = view.bounds
         
         lightDarkSwitch.addTarget(self, action: #selector(lightDarkSwitchChanged), for: .valueChanged)
         biometricSwitch.addTarget(self, action: #selector(biometricSwitchChanged), for: .valueChanged)
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        switch indexPath.row {
-            case 0:
-                cell.textLabel?.text = "Dark Mode"
-                cell.accessoryView = lightDarkSwitch
-            case 1:
-                cell.textLabel?.text = "Lock App"
-                cell.accessoryView = biometricSwitch
-            default:
-                break
-        }
-        
-        return cell
+        languageButton.addTarget(self, action: #selector(doChangeLanguage), for: .touchUpInside)
     }
     
     @objc func lightDarkSwitchChanged(_ sender: UISwitch) {
@@ -86,5 +78,97 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         let isDarkMode = UserDefaults.standard.bool(forKey: "isDarkMode")
         self.view.overrideUserInterfaceStyle = isDarkMode ? .dark : .light
         self.tabBarController?.overrideUserInterfaceStyle = isDarkMode ? .dark : .light
+    }
+    
+    @objc func doChangeLanguage() {
+        actionSheet = UIAlertController(title: nil, message: "alert_change_language".localized(), preferredStyle: UIAlertController.Style.actionSheet)
+        for language in availableLanguages {
+            if language != "Base" {
+                print("LANGUAGE ", language)
+                let displayName = Localize.displayNameForLanguage(language)
+                print("DISPLAYNAME ", displayName)
+                let languageAction = UIAlertAction(title: displayName, style: .default, handler: {
+                    (alert: UIAlertAction!) -> Void in
+                    Localize.setCurrentLanguage(language)
+                    self.languageButton.setTitle("\(displayName) ↓", for: .normal)
+                    UserDefaults.standard.set(displayName, forKey: "language")
+                })
+                actionSheet.addAction(languageAction)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "alert_cancel".localized(), style: UIAlertAction.Style.cancel, handler: {
+            (alert: UIAlertAction) -> Void in
+        })
+        actionSheet.addAction(cancelAction)
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+}
+
+// MARK: - UITableViewDelegate and UITableViewDataSource
+
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3  // Toplam bölüm sayısı
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+            case 0:
+                return 1
+            case 1:
+                return 1
+            case 2:
+                return 1
+            default:
+                return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+            case 0:
+                return "tab_settings_section_appearance".localized()
+            case 1:
+                return "tab_settings_section_security".localized()
+            case 2:
+                return "tab_settings_section_language".localized()
+            default:
+                return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        switch indexPath.section {
+            case 0:
+                if indexPath.row == 0 {
+                    cell.textLabel?.text = "tab_settings_dark_mode".localized()
+                    cell.accessoryView = lightDarkSwitch
+                }
+            case 1:
+                if indexPath.row == 0 {
+                    cell.textLabel?.text = "tab_settings_lock_app".localized()
+                    cell.accessoryView = biometricSwitch
+                }
+            case 2:
+                if indexPath.row == 0 {
+                    cell.textLabel?.text = "tab_settings_language".localized()
+                    cell.accessoryView = languageButton
+                }
+            default:
+                break
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 2 {
+            doChangeLanguage()
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
